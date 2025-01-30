ceca8b604744bdd27f64f39e72e791b79dd2e74e
ZhyrexCalamiong <xeryhzcalamiong@gmail.com>
2025-01-30 17:05:06 +0800

    rex
    gesture intro

--- lib/screens/get_started/get_started_page3.dart
+++ lib/screens/get_started/get_started_page3.dart
@@ -1,11 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
 import 'get_started_page4.dart';
 
-class GetStartedPage3 extends StatelessWidget {
+class GetStartedPage3 extends StatefulWidget {
+  @override
+  _GetStartedPage3State createState() => _GetStartedPage3State();
+}
+
+class _GetStartedPage3State extends State<GetStartedPage3> {
+  String detectedLabel = 'Waiting for gesture...';
+
+  // Function to send request to Python server and get the label
+  Future<void> detectGesture() async {
+    try {
+      final response = await http.get(Uri.parse('http://127.0.0.1:5000/detect'));
+
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> data = json.decode(response.body);
+        String label = data['label'];
+
+        setState(() {
+          detectedLabel = label;
+        });
+
+        // Navigate based on label using Navigator.push()
+        if (label == 'B') {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => GetStartedPage4()),  // Navigate to GetStartedPage4
+          );
+        }
+      } else {
+        throw Exception('Failed to detect gesture');
+      }
+    } catch (e) {
+      print("Error: $e");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFF273236),
+      appBar: AppBar(title: Text('Gesture Detection')),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -29,14 +68,11 @@ class GetStartedPage3 extends StatelessWidget {
               ),
             ),
             SizedBox(height: 40),
+            Text(detectedLabel, style: TextStyle(fontSize: 24)),
+            SizedBox(height: 40),
             ElevatedButton(
-              onPressed: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (context) => GetStartedPage4()),
-                );
-              },
-              child: Text("Next"),
+              onPressed: detectGesture,
+              child: Text('Detect Gesture'),
             ),
           ],
         ),
