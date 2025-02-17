ec7dee58e4d28c666bf77da9e357b3f4774945b8
YToh <galabayanthony92@gmail.com>
2025-02-18 00:43:36 +0800

    added transportation mappings and refactor its page

---
+++ frontend/lib/api/transportation.dart
@@ -0,0 +1,14 @@
+final Map<String, String> transportationMappings = {
+  "Eroplano": "LinguaAR/Gulmatico/Transportation/eroplano",
+  "Helicopter": "LinguaAR/Gulmatico/Transportation/helicopter",
+  "Jeep": "LinguaAR/Gulmatico/Transportation/jeep",
+  "Motorsiklo": "LinguaAR/Gulmatico/Transportation/motorsiklo",
+  "Truck ng Bumbero": "LinguaAR/Gulmatico/Transportation/truckngbumbero",
+  "Paliparan": "LinguaAR/Gulmatico/Transportation/paliparan",
+  "Kalesa": "LinguaAR/Gulmatico/Transportation/kalesa",
+  "Bus": "LinguaAR/Gulmatico/Transportation/bus",
+  "Bisikleta": "LinguaAR/Gulmatico/Transportation/bisikleta",
+  "Truck ng Basura": "LinguaAR/Gulmatico/Transportation/truckngbasura",
+  "Ambulansya": "LinguaAR/Gulmatico/Transportation/ambulansya",
+  "Hot air Balloon": "LinguaAR/Gulmatico/Transportation/hotairballon",
+};
