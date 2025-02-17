59f0df490fc26109596b8690e8f5e9e20a31a589
YToh <galabayanthony92@gmail.com>
2025-02-18 00:43:48 +0800

    added technology mappings and refactor its page

---
+++ frontend/lib/api/technology.dart
@@ -0,0 +1,7 @@
+final Map<String, String> technologyMappings = {
+  "Computer": "LinguaAR/Zhyrex/Technology/computer",
+  "Telebisiyon": "LinguaAR/Zhyrex/Technology/tv",
+  "Mop": "LinguaAR/Zhyrex/Technology/mop",
+  "Electric Fan": "LinguaAR/Zhyrex/Technology/electricfan",
+  "Laptop": "LinguaAR/Zhyrex/Technology/laptop",
+};
