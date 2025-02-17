83d884a8cebff9fe0ecc844587e42c03a2e0a7a2
YToh <galabayanthony92@gmail.com>
2025-02-18 00:42:54 +0800

    added food_drinks mappings and refactor its page

--- frontend/lib/fsl_guide/travel_food_environment/food_drinks_page.dart
+++ frontend/lib/fsl_guide/travel_food_environment/food_drinks_page.dart
@@ -1,258 +1,38 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
-import 'dart:async'; // For CancelToken
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lingua_arv1/api/food_drinks.dart'; 
+import 'package:lingua_arv1/bloc/Gif/gif_bloc.dart';
+import 'package:lingua_arv1/bloc/Gif/gif_event.dart';
+import 'package:lingua_arv1/bloc/Gif/gif_state.dart';
 
-class FoodDrinksPage extends StatefulWidget {
-  @override
-  State<FoodDrinksPage> createState() => _FoodDrinksPageState();
-}
+class FoodDrinksPage extends StatelessWidget { 
+  final List<String> phrases = fooddrinksMappings.keys.toList(); 
 
-class _FoodDrinksPageState extends State<FoodDrinksPage> {
-  String _gifUrl = '';
-  bool _isLoading = false;
-  String _errorMessage = '';
-  String _currentPhrase = ''; // New variable to store the current phrase
-  http.Client? _httpClient;
-  final List<String> phrases = [
-    "Kape",
-    "Cereal",
-    "Gutom",
-    "Hotdog",
-    "Agahan",
-    "Tsaa",
-    "Juice",
-    "Itlog",
-    "Pancake ",
-    "Ham",
-    'Tinapay',
-    'Inumin',
-    'Pagkain',
-    'Soda',
-    'Mantika',
-    'Tanghalian',
-    'Mayonnaise',
-    'Hapunan'
-  ];
-
-  @override
-  void dispose() {
-    // Cancel any ongoing requests
-    _httpClient?.close();
-    super.dispose();
-  }
-
-  Future<void> _fetchGif(String phrase) async {
-    setState(() {
-      _isLoading = true;
-      _errorMessage = '';
-      _gifUrl = ''; // Clear previous GIF
-      _currentPhrase = phrase; // Set the current phrase
-    });
-
-    String url = '';
-    String publicId = '';
-    String imageUrl = '';
-
-    switch (phrase) {
-      case "Kape":
-        imageUrl =
-            "https://res.cloudinary.com/dqthtm7gt/image/upload/v1739467789/LinguaAR/Anthony/Relasyon/Valentines-book.gif";
-        publicId = "LinguaAR/Gulmatico/Foods/kape";
-        break;
-      case "Cereal":
-        imageUrl =
-            "https://res.cloudinary.com/dqthtm7gt/image/upload/v1739467811/LinguaAR/Anthony/Relasyon/Valentines-card.gif";
-        publicId = "LinguaAR/Gulmatico/Foods/cereal";
-        break;
-      case "Gutom":
-        imageUrl =
-            "https://res.cloudinary.com/dqthtm7gt/image/upload/v1739467846/LinguaAR/Anthony/Relasyon/Valentines-day.gif";
-        publicId = "LinguaAR/Gulmatico/Foods/gutom";
-        break;
-      case "Hotdog":
-        imageUrl =
-            "https://res.cloudinary.com/dqthtm7gt/image/upload/v1739467884/LinguaAR/Anthony/Relasyon/Bulaklak.gif";
-        publicId = "LinguaAR/Gulmatico/Foods/hotdog";
-        break;
-      case "Agahan":
-        imageUrl =
-            "https://res.cloudinary.com/dqthtm7gt/image/upload/v1739467903/LinguaAR/Anthony/Relasyon/Halik.gif";
-        publicId = "LinguaAR/Gulmatico/Foods/agahan";
-        break;
-      case "Tsaa":
-        imageUrl =
-            "https://res.cloudinary.com/dqthtm7gt/image/upload/v1739467903/LinguaAR/Anthony/Relasyon/Halik.gif";
-        publicId = "LinguaAR/Gulmatico/Foods/tsaa";
-        break;
-      case "Juice":
-        imageUrl =
-            "https://res.cloudinary.com/dqthtm7gt/image/upload/v1739467903/LinguaAR/Anthony/Relasyon/Halik.gif";
-        publicId = "LinguaAR/Gulmatico/Foods/juice";
-        break;
-      case "Itlog":
-        imageUrl =
-            "https://res.cloudinary.com/dqthtm7gt/image/upload/v1739467903/LinguaAR/Anthony/Relasyon/Halik.gif";
-        publicId = "LinguaAR/Gulmatico/Foods/itlog";
-        break;
-      case "Pancake":
-        imageUrl =
-            "https://res.cloudinary.com/dqthtm7gt/image/upload/v1739467903/LinguaAR/Anthony/Relasyon/Halik.gif";
-        publicId = "LinguaAR/Gulmatico/Foods/pancake";
-        break;
-      case "Ham":
-        imageUrl =
-            "https://res.cloudinary.com/dqthtm7gt/image/upload/v1739467903/LinguaAR/Anthony/Relasyon/Halik.gif";
-        publicId = "LinguaAR/Gulmatico/Foods/ham";
-        break;
-      case "Tinapay":
-        imageUrl =
-            "https://res.cloudinary.com/dqthtm7gt/image/upload/v1739467903/LinguaAR/Anthony/Relasyon/Halik.gif";
-        publicId = "LinguaAR/Gulmatico/Foods/tinapay";
-        break;
-      case "Inumin":
-        imageUrl =
-            "https://res.cloudinary.com/dqthtm7gt/image/upload/v1739467903/LinguaAR/Anthony/Relasyon/Halik.gif";
-        publicId = "LinguaAR/Gulmatico/Foods/inumin";
-        break;
-      case "Pagkain":
-        imageUrl =
-            "https://res.cloudinary.com/dqthtm7gt/image/upload/v1739467903/LinguaAR/Anthony/Relasyon/Halik.gif";
-        publicId = "LinguaAR/Gulmatico/Foods/pagkain";
-        break;
-      case "Soda":
-        imageUrl =
-            "https://res.cloudinary.com/dqthtm7gt/image/upload/v1739467903/LinguaAR/Anthony/Relasyon/Halik.gif";
-        publicId = "LinguaAR/Gulmatico/Foods/soda";
-        break;
-      case "Mantika":
-        imageUrl =
-            "https://res.cloudinary.com/dqthtm7gt/image/upload/v1739467903/LinguaAR/Anthony/Relasyon/Halik.gif";
-        publicId = "LinguaAR/Gulmatico/Foods/mantika";
-        break;
-      case "Tanghalian":
-        imageUrl =
-            "https://res.cloudinary.com/dqthtm7gt/image/upload/v1739467903/LinguaAR/Anthony/Relasyon/Halik.gif";
-        publicId = "LinguaAR/Gulmatico/Foods/tanghalian";
-        break;
-      case "Mayonnaise":
-        imageUrl =
-            "https://res.cloudinary.com/dqthtm7gt/image/upload/v1739467903/LinguaAR/Anthony/Relasyon/Halik.gif";
-        publicId = "LinguaAR/Gulmatico/Foods/mayonaise";
-        break;
-      case "Hapunan":
-        imageUrl =
-            "https://res.cloudinary.com/dqthtm7gt/image/upload/v1739467903/LinguaAR/Anthony/Relasyon/Halik.gif";
-        publicId = "LinguaAR/Gulmatico/Foods/hapunan";
-        break;
-      default:
-        setState(() {
-          _isLoading = false;
-          _errorMessage = 'No GIF found for this phrase.';
-        });
-        return;
-    }
-
-    if (publicId.isNotEmpty) {
-      url = 'http://192.168.157.7:5000/cloudinary/get_gif?public_id=$publicId';
-    } else if (imageUrl.isNotEmpty) {
-      url = 'http://192.168.157.7:5000/cloudinary/get_gif?url=$imageUrl';
-    }
-
-    _httpClient = http.Client(); // Initialize HTTP client
-
-    try {
-      final response = await _httpClient!.get(Uri.parse(url)).timeout(
-        Duration(seconds: 10),
-        onTimeout: () {
-          throw TimeoutException('Request timed out');
-        },
-      );
-
-      if (response.statusCode == 200) {
-        var jsonResponse = jsonDecode(response.body);
-        setState(() {
-          _gifUrl = jsonResponse['gif_url'];
-          _isLoading = false;
-        });
-      } else {
-        setState(() {
-          _isLoading = false;
-          _errorMessage = 'Failed to fetch GIF from server.';
-        });
-      }
-    } catch (error) {
-      if (error is TimeoutException) {
-        setState(() {
-          _isLoading = false;
-          _errorMessage = 'Request timed out. Please try again.';
-        });
-      } else {
-        setState(() {
-          _isLoading = false;
-          _errorMessage = 'Error occurred: $error';
-        });
-      }
-    } finally {
-      _httpClient?.close(); // Close the HTTP client
-    }
-  }
-
-  void _showGifPopup(BuildContext context) {
+  void _showGifPopup(BuildContext context, String phrase, String gifUrl) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
           content: Column(
-            mainAxisSize: MainAxisSize.min, // Use minimum space
+            mainAxisSize: MainAxisSize.min,
             children: [
-              // Text above the container
               Text(
-                _currentPhrase,
+                phrase,
                 style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
               ),
-              SizedBox(height: 10), // Add some spacing
-              // Container for the GIF
+              SizedBox(height: 10),
               Container(
-                width: 500, // Fixed width for the container
-                height: 410, // Fixed height for the container
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey),
-                ),
-                child: Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    if (_gifUrl.isNotEmpty)
-                      Image.network(
-                        _gifUrl,
-                        fit: BoxFit
-                            .cover, // Ensure the GIF fits inside the container
-                      ),
-                    if (_isLoading)
-                      CircularProgressIndicator(), // Show loading indicator
-                    if (_errorMessage.isNotEmpty)
-                      Center(
-                        child: Text(
-                          _errorMessage,
-                          style: TextStyle(color: Colors.red),
-                        ),
-                      ),
-                    if (_gifUrl.isEmpty && !_isLoading && _errorMessage.isEmpty)
-                      Center(
-                        child: Text(
-                          'GIF will appear here',
-                          style: TextStyle(color: Colors.grey),
-                        ),
-                      ),
-                  ],
-                ),
+                width: 500,
+                height: 410,
+                decoration: BoxDecoration(border: Border.all(color: Colors.grey)),
+                child: Image.network(gifUrl, fit: BoxFit.cover),
               ),
             ],
           ),
           actions: [
             TextButton(
               onPressed: () {
-                Navigator.of(context).pop(); // Close the pop-up
+                Navigator.of(context).pop();
               },
               child: Text('Back'),
             ),
@@ -264,30 +44,49 @@ class _FoodDrinksPageState extends State<FoodDrinksPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Foods and Drinks'),
-        centerTitle: true,
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () {
-            // Cancel ongoing requests and pop the page
-            _httpClient?.close();
-            Navigator.of(context).pop();
+    return BlocProvider(
+      create: (context) => GifBloc(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Food and Drinks'), 
+          centerTitle: true,
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ),
+        body: ListView.builder(
+          itemCount: phrases.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(phrases[index]),
+              onTap: () {
+                String phrase = phrases[index];
+                String publicId = fooddrinksMappings[phrase] ?? '';
+
+                if (publicId.isNotEmpty) {
+                  context.read<GifBloc>().add(FetchGif(phrase: phrase, publicId: publicId));
+                }
+              },
+            );
           },
         ),
-      ),
-      body: ListView.builder(
-        itemCount: phrases.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(phrases[index]),
-            onTap: () async {
-              await _fetchGif(phrases[index]); // Fetch the GIF
-              _showGifPopup(context); // Show the pop-up
-            },
-          );
-        },
+        bottomSheet: BlocBuilder<GifBloc, GifState>(
+          builder: (context, state) {
+            if (state is GifLoading) {
+              return Center(child: CircularProgressIndicator());
+            } else if (state is GifLoaded) {
+              WidgetsBinding.instance.addPostFrameCallback((_) {
+                _showGifPopup(context, state.phrase, state.gifUrl);
+              });
+            } else if (state is GifError) {
+              return Center(child: Text(state.message, style: TextStyle(color: Colors.red)));
+            }
+            return SizedBox.shrink();
+          },
+        ),
       ),
     );
   }
