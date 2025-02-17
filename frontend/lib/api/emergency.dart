2fcd79b0ed32ff452150bc39d8032b76d2d88f8c
YToh <galabayanthony92@gmail.com>
2025-02-18 00:42:06 +0800

    added Emergency and Nature Disaster mappings and refactor page

---
+++ frontend/lib/api/emergency.dart
@@ -0,0 +1,13 @@
+final Map<String, String> emergencyNatureMappings = {
+  "Baha": "LinguaAR/Gulmatico/Nature/baha",
+  "Magnanakaw": "LinguaAR/Gulmatico/Nature/magnanakaw",
+  "Sunog": "LinguaAR/Gulmatico/Nature/sunog",
+  "Tumawag ka ng tulong": "LinguaAR/Gulmatico/Nature/tumawagngtulong",
+  "Rescue": "LinguaAR/Gulmatico/Nature/rescue",
+  "Tulungan mo ako": "LinguaAR/Gulmatico/Nature/tulunganmoako",
+  "Lindol": "LinguaAR/Gulmatico/Nature/lindol",
+  "Emergency": "LinguaAR/Gulmatico/Nature/emergency",
+  "Tulungan mo sila": "LinguaAR/Gulmatico/Nature/tulunganmosila",
+  "Ulan": "LinguaAR/Gulmatico/Nature/ulan",
+  "Bagyo": "LinguaAR/Gulmatico/Nature/bagyo",
+};
