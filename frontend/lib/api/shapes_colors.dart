d16dcbcf4932eda5749d3ba4afc80cad43fcad05
YToh <galabayanthony92@gmail.com>
2025-02-18 00:43:21 +0800

    added shapes_colors mappings and refactor its page

---
+++ frontend/lib/api/shapes_colors.dart
@@ -0,0 +1,12 @@
+final Map<String, String> shapeColorsMappings = {
+  "Asul": "LinguaAR/Gulmatico/colorsandshapes/asul",
+  "Itim": "LinguaAR/Gulmatico/colorsandshapes/itim",
+  "Kayumanggi": "LinguaAR/Gulmatico/colorsandshapes/kayumanggi",
+  "Rosas": "LinguaAR/Gulmatico/colorsandshapes/rosas",
+  "Kahel": "LinguaAR/Gulmatico/colorsandshapes/kahel",
+  "Abo": "LinguaAR/Gulmatico/colorsandshapes/abo",
+  "Berde": "LinguaAR/Gulmatico/colorsandshapes/berde",
+  "Ginto": "LinguaAR/Gulmatico/colorsandshapes/ginto",
+  "Paresukat": "LinguaAR/Gulmatico/colorsandshapes/paresukat",
+  "Bilog": "LinguaAR/Gulmatico/colorsandshapes/bilog",
+};
