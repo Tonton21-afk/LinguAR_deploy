24e4f1a4c8230433cfc8b7e06f2873ff59aef03d
YToh <galabayanthony92@gmail.com>
2025-02-18 00:43:59 +0800

    added work profession mappings and refactor its page

---
+++ frontend/lib/api/work_profession.dart
@@ -0,0 +1,13 @@
+final Map<String, String> workProfessionMappings = {
+  "Mananayaw": "LinguaAR/Zhyrex/Jobs/dancer",
+  "Magsasaka": "LinguaAR/Zhyrex/Jobs/magsasaka",
+  "Karpentero": "LinguaAR/Zhyrex/Jobs/karpentero",
+  "Piloto": "LinguaAR/Zhyrex/Jobs/piloto",
+  "Electrician": "LinguaAR/Zhyrex/Jobs/electrician",
+  "Nars": "LinguaAR/Zhyrex/Jobs/nars",
+  "Doctor": "LinguaAR/Zhyrex/Jobs/doctor",
+  "Dentista": "LinguaAR/Zhyrex/Jobs/dentista",
+  "Bumbero": "LinguaAR/Zhyrex/Jobs/bumbero",
+  "Singer": "LinguaAR/Zhyrex/Jobs/singer",
+  "Pulis": "LinguaAR/Zhyrex/Jobs/pulis",
+};
--- frontend/lib/fsl_guide/learning_work_technology/work_profession_page.dart
+++ frontend/lib/fsl_guide/learning_work_technology/work_profession_page.dart
@@ -1,216 +1,38 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
-import 'dart:async'; // For CancelToken
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lingua_arv1/api/work_profession.dart'; 
+import 'package:lingua_arv1/bloc/Gif/gif_bloc.dart';
+import 'package:lingua_arv1/bloc/Gif/gif_event.dart';
+import 'package:lingua_arv1/bloc/Gif/gif_state.dart';
 
-class WorkProfessionPage extends StatefulWidget {
-  @override
-  State<WorkProfessionPage> createState() => _WorkProfessionPageState();
-}
+class WorkProfessionPage extends StatelessWidget { 
+  final List<String> phrases = workProfessionMappings.keys.toList(); 
 
-class _WorkProfessionPageState extends State<WorkProfessionPage> {
-  String _gifUrl = '';
-  bool _isLoading = false;
-  String _errorMessage = '';
-  String _currentPhrase = ''; // New variable to store the current phrase
-  http.Client? _httpClient;
-  final List<String> phrases = [
-    "Mananayaw",
-    "Magsasaka",
-    "Karpentero",
-    "Piloto",
-    "Electrician",
-    "Nars",
-    "Doctor",
-    "Dentista",
-    "Bumbero",
-    "Singer",
-    "Pulis",
-  ];
-
- @override
-  void dispose() {
-    // Cancel any ongoing requests
-    _httpClient?.close();
-    super.dispose();
-  }
-
-  Future<void> _fetchGif(String phrase) async {
-    setState(() {
-      _isLoading = true;
-      _errorMessage = '';
-      _gifUrl = ''; // Clear previous GIF
-      _currentPhrase = phrase; // Set the current phrase
-    });
-
-    String url = '';
-    String publicId = '';
-    String imageUrl = '';
-
-    switch (phrase) {
-      case "Mananayaw":
-        imageUrl =
-            "https://res.cloudinary.com/dqthtm7gt/image/upload/v1739467789/LinguaAR/Anthony/Relasyon/Valentines-book.gif";
-        publicId = "LinguaAR/Zhyrex/Jobs/dancer";
-        break;
-      case "Magsasakaa":
-        imageUrl =
-            "https://res.cloudinary.com/dqthtm7gt/image/upload/v1739467811/LinguaAR/Anthony/Relasyon/Valentines-card.gif";
-        publicId = "LinguaAR/Zhyrex/Jobs/magsasaka";
-        break;
-      case "Karpentero":
-        imageUrl =
-            "https://res.cloudinary.com/dqthtm7gt/image/upload/v1739467846/LinguaAR/Anthony/Relasyon/Valentines-day.gif";
-        publicId = "LinguaAR/Zhyrex/Jobs/karpentero";
-        break;
-      case "Piloto":
-        imageUrl =
-            "https://res.cloudinary.com/dqthtm7gt/image/upload/v1739467884/LinguaAR/Anthony/Relasyon/Bulaklak.gif";
-        publicId = "LinguaAR/Zhyrex/Jobs/piloto";
-        break;
-      case "Electrician":
-        imageUrl =
-            "https://res.cloudinary.com/dqthtm7gt/image/upload/v1739467903/LinguaAR/Anthony/Relasyon/Halik.gif";
-        publicId = "LinguaAR/Zhyrex/Jobs/electrician";
-        break;
-      case "Nars":
-        imageUrl =
-            "https://res.cloudinary.com/dqthtm7gt/image/upload/v1739467827/LinguaAR/Anthony/Relasyon/Nobyo.gif";
-        publicId = "LinguaAR/Zhyrex/Jobs/nars";
-        break;
-      case "Doctor":
-        imageUrl =
-            "https://res.cloudinary.com/dqthtm7gt/image/upload/v1739467925/LinguaAR/Anthony/Relasyon/Nobya.gif";
-        publicId = "LinguaAR/Zhyrex/Jobs/doctor";
-        break;
-      case "Dentista":
-        imageUrl =
-            "https://res.cloudinary.com/dqthtm7gt/image/upload/v1739467925/LinguaAR/Anthony/Relasyon/Nobya.gif";
-        publicId = "LinguaAR/Zhyrex/Jobs/dentista";
-        break;
-      case "Bumbero":
-        imageUrl =
-            "https://res.cloudinary.com/dqthtm7gt/image/upload/v1739467940/LinguaAR/Anthony/Relasyon/Mahal-kita.gif";
-        publicId = "LinguaAR/Zhyrex/Jobs/bumbero";
-        break;
-      case "Singer":
-        imageUrl =
-            "https://res.cloudinary.com/dqthtm7gt/image/upload/v1739467940/LinguaAR/Anthony/Relasyon/Mahal-kita.gif";
-        publicId = "LinguaAR/Zhyrex/Jobs/singer";
-        break;
-      case "Pulis":
-        imageUrl =
-            "https://res.cloudinary.com/dqthtm7gt/image/upload/v1739467940/LinguaAR/Anthony/Relasyon/Mahal-kita.gif";
-        publicId = "LinguaAR/Zhyrex/Education/pulis";
-        break;
-      default:
-        setState(() {
-          _isLoading = false;
-          _errorMessage = 'No GIF found for this phrase.';
-        });
-        return;
-    }
-
-    if (publicId.isNotEmpty) {
-      url = 'http://192.168.157.7:5000/cloudinary/get_gif?public_id=$publicId';
-    } else if (imageUrl.isNotEmpty) {
-      url = 'http://192.168.157.7:5000/cloudinary/get_gif?url=$imageUrl';
-    }
-
-    _httpClient = http.Client(); // Initialize HTTP client
-
-    try {
-      final response = await _httpClient!.get(Uri.parse(url)).timeout(
-        Duration(seconds: 10),
-        onTimeout: () {
-          throw TimeoutException('Request timed out');
-        },
-      );
-
-      if (response.statusCode == 200) {
-        var jsonResponse = jsonDecode(response.body);
-        setState(() {
-          _gifUrl = jsonResponse['gif_url'];
-          _isLoading = false;
-        });
-      } else {
-        setState(() {
-          _isLoading = false;
-          _errorMessage = 'Failed to fetch GIF from server.';
-        });
-      }
-    } catch (error) {
-      if (error is TimeoutException) {
-        setState(() {
-          _isLoading = false;
-          _errorMessage = 'Request timed out. Please try again.';
-        });
-      } else {
-        setState(() {
-          _isLoading = false;
-          _errorMessage = 'Error occurred: $error';
-        });
-      }
-    } finally {
-      _httpClient?.close(); // Close the HTTP client
-    }
-  }
-
-  void _showGifPopup(BuildContext context) {
+  void _showGifPopup(BuildContext context, String phrase, String gifUrl) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
           content: Column(
-            mainAxisSize: MainAxisSize.min, // Use minimum space
+            mainAxisSize: MainAxisSize.min,
             children: [
-              // Text above the container
               Text(
-                _currentPhrase,
+                phrase,
                 style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
               ),
-              SizedBox(height: 10), // Add some spacing
-              // Container for the GIF
+              SizedBox(height: 10),
               Container(
-                width: 500, // Fixed width for the container
-                height: 410, // Fixed height for the container
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey),
-                ),
-                child: Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    if (_gifUrl.isNotEmpty)
-                      Image.network(
-                        _gifUrl,
-                        fit: BoxFit
-                            .cover, // Ensure the GIF fits inside the container
-                      ),
-                    if (_isLoading)
-                      CircularProgressIndicator(), // Show loading indicator
-                    if (_errorMessage.isNotEmpty)
-                      Center(
-                        child: Text(
-                          _errorMessage,
-                          style: TextStyle(color: Colors.red),
-                        ),
-                      ),
-                    if (_gifUrl.isEmpty && !_isLoading && _errorMessage.isEmpty)
-                      Center(
-                        child: Text(
-                          'GIF will appear here',
-                          style: TextStyle(color: Colors.grey),
-                        ),
-                      ),
-                  ],
-                ),
+                width: 500,
+                height: 410,
+                decoration: BoxDecoration(border: Border.all(color: Colors.grey)),
+                child: Image.network(gifUrl, fit: BoxFit.cover),
               ),
             ],
           ),
           actions: [
             TextButton(
               onPressed: () {
-                Navigator.of(context).pop(); // Close the pop-up
+                Navigator.of(context).pop();
               },
               child: Text('Back'),
             ),
@@ -222,30 +44,49 @@ class _WorkProfessionPageState extends State<WorkProfessionPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Work Profession'),
-        centerTitle: true,
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () {
-            // Cancel ongoing requests and pop the page
-            _httpClient?.close();
-            Navigator.of(context).pop();
+    return BlocProvider(
+      create: (context) => GifBloc(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Work Profession'), 
+          centerTitle: true,
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ),
+        body: ListView.builder(
+          itemCount: phrases.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(phrases[index]),
+              onTap: () {
+                String phrase = phrases[index];
+                String publicId = workProfessionMappings[phrase] ?? ''; 
+
+                if (publicId.isNotEmpty) {
+                  context.read<GifBloc>().add(FetchGif(phrase: phrase, publicId: publicId));
+                }
+              },
+            );
           },
         ),
-      ),
-      body: ListView.builder(
-        itemCount: phrases.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(phrases[index]),
-            onTap: () async {
-              await _fetchGif(phrases[index]); // Fetch the GIF
-              _showGifPopup(context); // Show the pop-up
-            },
-          );
-        },
+        bottomSheet: BlocBuilder<GifBloc, GifState>(
+          builder: (context, state) {
+            if (state is GifLoading) {
+              return Center(child: CircularProgressIndicator());
+            } else if (state is GifLoaded) {
+              WidgetsBinding.instance.addPostFrameCallback((_) {
+                _showGifPopup(context, state.phrase, state.gifUrl);
+              });
+            } else if (state is GifError) {
+              return Center(child: Text(state.message, style: TextStyle(color: Colors.red)));
+            }
+            return SizedBox.shrink();
+          },
+        ),
       ),
     );
   }
