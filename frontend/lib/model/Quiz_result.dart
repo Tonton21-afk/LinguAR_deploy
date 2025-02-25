0467929b5f6d0ffa0dbadb379e340105ac2f4ed0
YToh <galabayanthony92@gmail.com>
2025-02-26 03:05:46 +0800

    ensure the restart function works for the current category.

---
+++ frontend/lib/model/Quiz_result.dart
@@ -0,0 +1,11 @@
+class QuizResult {
+  final int correctAnswers;
+  final int wrongAnswers;
+  final String category; 
+
+  QuizResult({
+    required this.correctAnswers,
+    required this.wrongAnswers,
+    required this.category, 
+  });
+}
